3c05b62223c1d585010eda87d593ec09abe6f6de
Harshita <thelastjedi@users.noreply.github.com>
2015-11-28 22:25:25 +0530

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+$(function() {
+
+    // Populate using an object literal
+    WTF.init({
+
+        heading: [
+            "heading",
+        ],
+        template: [
+            "@first",
+            "@second",
+        ],
+        first: [
+            "first",
+        ],
+        second: [
+            "second",
+        ],
+        response: [
+            "Again",
+        ]
+    });
+
+});
